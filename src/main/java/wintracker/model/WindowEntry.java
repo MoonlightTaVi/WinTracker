8f52bd8078bc8dd02fb52b2230f0f990d7200ffb
Moonlight <moonlight9512@gmail.com>
2025-07-03 18:08:27 +0500

    Add default values; override equals/hashcode

--- src/main/java/wintracker/model/WindowEntry.java
+++ src/main/java/wintracker/model/WindowEntry.java
@@ -14,15 +14,45 @@ public class WindowEntry {
 	private Long id;
 	@Setter
 	@Getter
-	private String title;
+	private String title = "<nil>";
 	@Setter
 	@Getter
-	private Integer secondsOpened;
+	private Integer secondsOpened = 0;
+	@Getter
+	private LocalDateTime createdDate = LocalDateTime.now();
 	@Setter
 	@Getter
-	private LocalDateTime lastDate;
+	private LocalDateTime lastDate = LocalDateTime.now();
 	@Setter
 	@Getter
 	private String meta;
+	
+	public WindowEntry() {}
+	public WindowEntry(String title) {
+		this.title = title;
+	}
+	
+	public void updateFrom(WindowEntry anotherEntry) {
+		id = anotherEntry.id;
+		title = anotherEntry.title;
+		secondsOpened = anotherEntry.secondsOpened;
+		createdDate = anotherEntry.createdDate;
+		lastDate = anotherEntry.lastDate;
+		meta = anotherEntry.meta;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		boolean equals = false;
+		if (o instanceof WindowEntry e) {
+			equals = id.equals(e.id);
+		}
+		return equals;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id.intValue();
+	}
 	
 }
